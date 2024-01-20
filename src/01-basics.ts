1e9256809eb9a187cadfe17efc5c9f937f1a6f1f
Martin <20106234@mail.wit.ie>
2024-01-20 12:03:01 +0100

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -15,23 +15,28 @@ interface Colleague {
     },
 };
 
-const friend1: Friend = {
+interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+}
+
+const friend1 : Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
     age: 25,
   };
   
-  const friend2: Friend = {
+  const friend2 : Friend = {
     name: "Jane Costello",
     phone: "086--12345",
     age: 31,
   };
   
-  const friends: Friend[] = [friend1, friend2];
+  const friends : Friend[] = [friend1, friend2];
   console.log(friends[1]);
   
   //   -------------------
-  const colleague1: Colleague = {
+  const colleague1 : Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -40,7 +45,7 @@ const friend1: Friend = {
     },
   };
   
-  const colleague2: Colleague = {
+  const colleague2 : Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -49,7 +54,7 @@ const friend1: Friend = {
     },
   };
   
-  const colleague3: Colleague = {
+  const colleague3 : Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
@@ -57,7 +62,7 @@ const friend1: Friend = {
       extension: 125,
     },
   };
-  const colleagues = {
+  const colleagues : ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
